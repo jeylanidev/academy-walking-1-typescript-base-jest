f87ed10fbaf9c50d65d11295dbaa818682af4a61
Amir Sohi <amir.sohi@cazoo.co.uk>
2021-09-21 14:16:11 +0100

    mob next [ci-skip]

--- src/week-14/main/app.ts
+++ src/week-14/main/app.ts
@@ -1,5 +1,18 @@
 export class App {
-    execute() {
-        console.log("Date || Amount || Balance")
-    }
+  execute() {
+    const bank = new Bank();
+    console.log("Date || Amount || Balance");
+    console.log(`10/01/2021 || 1000 || ${bank.deposit(1000)}`);
+  }
+}
+
+class Bank {
+  private balance: number;
+  constructor(amount: number = 0) {
+    this.balance = amount;
+  }
+  deposit(amount: number): number {
+    this.balance += amount;
+    return this.balance;
+  }
 }
--- src/week-14/test/app.test.ts
+++ src/week-14/test/app.test.ts
@@ -1,6 +1,5 @@
 import { App } from "../main/app";
 
-
 describe("Bank", () => {
   const consoleLog = jest.spyOn(console, "log").mockImplementation();
 
@@ -18,6 +17,13 @@ describe("Bank", () => {
     const app = new App();
     app.execute();
 
-    expect(consoleLog).toHaveBeenCalledWith("10/01/2012 || 1000 || 1000");
+    expect(consoleLog).toHaveBeenCalledWith("10/01/2021 || 1000 || 1000");
+  });
+
+  it("should make withdrawal and print leftover balance", () => {
+    const app = new App();
+    app.execute();
+
+    expect(consoleLog).toHaveBeenCalledWith("10/01/2021 || -500 || 500");
   });
 });
